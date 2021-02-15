d529dfbbf46ccfd76989b746b872cfdb77cbe70b
Italo Pires <italonapalm@gmail.com>
2021-02-15 12:08:17 -0300

    added sizes to upload pictures and I18n

--- app/uploaders/citizen_uploader.rb
+++ app/uploaders/citizen_uploader.rb
@@ -1,7 +1,23 @@
 class CitizenUploader < CarrierWave::Uploader::Base
-  storage :file
+  include CarrierWave::MiniMagick
 
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  end
+
+  def extension_whitelist
+    %w(jpg jpeg gif png)
+  end
+
+  version :thumb do
+    process resize_to_fit: [100,100]
+  end
+
+  version :medium do
+    process resize_to_fit: [300,300]
+  end
+
+  version :large do
+    process resize_to_fit: [800,800]
   end
 end
