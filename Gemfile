af850a38339a053281d80740ce14f3de2571052d
Italo Pires <italonapalm@gmail.com>
2021-02-15 11:26:46 -0300

    Added cpf validator

--- Gemfile
+++ Gemfile
@@ -39,6 +39,7 @@ gem 'bootsnap', '>= 1.1.0', require: false
 gem 'devise'
 gem 'jquery-rails'
 gem 'simple_form'
+gem 'cpf_cnpj'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/models/citizen.rb
+++ app/models/citizen.rb
@@ -1,4 +1,15 @@
 class Citizen < ApplicationRecord
   validates :full_name, :email, :birthdate, :phone, presence: true
   validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }, presence: true
+  validate :cpf_validator
+
+  def cpf=(cpf)
+    write_attribute(:cpf, CPF.new(cpf).stripped)
+  end
+
+  def cpf_validator
+    return if CPF.valid?(cpf)
+
+    errors.add(:cpf, "CPF é inválido")
+  end
 end
