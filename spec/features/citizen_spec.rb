93c0d2204caffcbf6640efcc43b0cdc6b6fcfc46
Italo Pires <italonapalm@gmail.com>
2021-02-16 13:24:15 -0300

    fixed citizen feature test

--- spec/features/citizen_spec.rb
+++ spec/features/citizen_spec.rb
@@ -2,6 +2,11 @@ require 'rails_helper'
 
 RSpec.feature 'Citizens' do
   given(:user) { create(:user) }
+  given!(:state1) { create(:state, name: 'State1') }
+  given!(:state2) { create(:state, name: 'State2') }
+  given!(:city1) { create(:city, name: 'City1', state: state1) }
+  given!(:city2) { create(:city, name: 'City2', state: state1) }
+  given!(:city3) { create(:city, name: 'City3', state: state2) }
 
   before { login_as user }
 
@@ -15,15 +20,24 @@ RSpec.feature 'Citizens' do
     fill_in Citizen.human_attribute_name('cpf'), with: '890.764.140-41'
     fill_in Citizen.human_attribute_name('email'), with: 'email@email.com'
     fill_in Citizen.human_attribute_name('birthdate'), with: I18n.l(Date.current.advance(months: -12))
-    fill_in Citizen.human_attribute_name('phone'), with: '8632222222'
     page.execute_script('$(".datepicker-dropdown").css({left: 0});')
+    fill_in Citizen.human_attribute_name('phone'), with: '8633333333'
+    attach_file Citizen.human_attribute_name('picture_file'), "#{Rails.root}/spec/fixtures/om30.png"
+    select state1.name, from: Address.human_attribute_name('state_id')
+    select city2.name, from: Address.human_attribute_name('city_id')
+    fill_in Address.human_attribute_name('zipcode'), with: '64000000'
+    fill_in Address.human_attribute_name('district'), with: 'district_1'
+    fill_in Address.human_attribute_name('public_place'), with: 'public_place1'
+    fill_in Address.human_attribute_name('complement'), with: 'complement1'
+    fill_in Address.human_attribute_name('ibge_code'), with: '123456'
+
     click_button 'Salvar'
 
     expect(page).to have_content 'Munícipe adicionado(a) com sucesso.'
     expect(page).to have_content '890.764.140-41'
     expect(page).to have_content 'email@email.com'
     expect(page).to have_content I18n.l(Date.current.advance(months: -12))
-    expect(page).to have_content '(86)32222222'
+    expect(page).to have_content '(86)33333333'
     find('#edit').click
 
     fill_in Citizen.human_attribute_name('full_name'), with: 'Munícipe 2'
@@ -38,9 +52,7 @@ RSpec.feature 'Citizens' do
     expect(page).to have_content 'email2@email.com'
     expect(page).to have_content I18n.l(Date.current.advance(months: -24))
 
-    accept_confirm do
-      find('#destroy').click
-    end
+    accept_confirm { find('#destroy').click }
 
     expect(page).to have_content 'Munícipe removido(a) com sucesso.'
     expect(page).not_to have_content 'Munícipe 2'
