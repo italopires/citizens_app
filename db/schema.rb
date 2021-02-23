805352388859f9ca6e5c451f2b7aac1a50468e9a
Italo Pires <italonapalm@gmail.com>
2021-02-23 14:49:17 -0300

    perf(schema.rb): added indexes
    added indexes to citizens table to get better performance with sql query

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_16_125911) do
+ActiveRecord::Schema.define(version: 2021_02_23_174214) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,6 +48,9 @@ ActiveRecord::Schema.define(version: 2021_02_16_125911) do
     t.datetime "updated_at", null: false
     t.string "picture_file"
     t.bigint "user_id"
+    t.index ["cpf"], name: "index_citizens_on_cpf"
+    t.index ["email"], name: "index_citizens_on_email"
+    t.index ["full_name"], name: "index_citizens_on_full_name"
     t.index ["user_id"], name: "index_citizens_on_user_id"
   end
 
